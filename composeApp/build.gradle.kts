a4e07a5f4dd9672408be853f0f5e84f0c2bba9f0
Shengyou Fan <shengyoufan@gmail.com>
2023-12-05 15:54:57 +0800

    project init

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    jvm("desktop")
+    
+    sourceSets {
+        val desktopMain by getting
+        
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.jetbrains.game"
+            packageVersion = "1.0.0"
+        }
+    }
+}
